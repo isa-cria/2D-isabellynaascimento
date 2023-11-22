37fe76beaa3a22a40adfa3e6f6a565fdeefa4298
isa-cria <111918275+isa-cria@users.noreply.github.com>
2023-11-22 12:47:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const aplausos = ducument,queryselector("#som tela aplausos;")
+function tocasom(som){
+       alert(som);
+}
+tocasom(clap, clap, clap); 
+let controle = "tocar;"
+if (controle == "tocar"){
+    alert("Aúdio pronto para tocar")
+}else{
+    alert("Aúdio não esta pronto")
+}
+const buttons=ducment.querySelectorAll("button")
+let contador=0
+while (contador< buttons.lenght){
+    buttons[contador].textContent=contador;
+    contador=contador+1
+}
